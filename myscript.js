d136f877e9a66bcbb1227c31bc4af8dcb9786715
chumbah <116595613+chumbah@users.noreply.github.com>
2024-01-30 13:59:30 +0300

    p

---
+++ myscript.js
@@ -0,0 +1,5 @@
+let amount_el = document.getElementById('amount').value
+let sub_el = document.getElementById('submit-btn')
+ function submit() {
+    console.log("payment of" + amount.el + "is successful.")
+ }
