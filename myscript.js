04464a06ec493ec2da6d7b39529120d21f842bb3
chumbah <116595613+chumbah@users.noreply.github.com>
2024-02-16 17:10:50 +0300

    constrain update

--- myscript.js
+++ myscript.js
@@ -1,6 +1,58 @@
 function submit(){
    alert("Done!")
 }
+// inputFormat.js
+
+function formatInput(input) {
+    // Remove non-numeric characters
+    var inputValue = input.value.replace(/\D/g, '');
+
+    // Apply specific formatting rules based on input ID
+    if (input.id === 'cnumber') {
+        // Limit to 16 characters for the first input
+        if (inputValue.length > 16) {
+            inputValue = inputValue.substr(0, 16);
+        }
+
+        // Add hyphens after every 4 characters
+        var formattedValue = '';
+        for (var i = 0; i < inputValue.length; i++) {
+            if (i > 0 && i % 4 === 0) {
+                formattedValue += '-';
+            }
+            formattedValue += inputValue[i];
+        }
+        input.value = formattedValue;
+    } else if (input.id === 'exp') {
+        // Limit to 4 characters for the second input
+        if (inputValue.length > 4) {
+            inputValue = inputValue.substr(0, 4);
+        }
+
+        // Add backslashes after every 2 characters
+        var formattedValue = '';
+        for (var i = 0; i < inputValue.length; i++) {
+            if (i > 0 && i % 2 === 0) {
+                formattedValue += '/';
+            }
+            formattedValue += inputValue[i];
+        }
+        input.value = formattedValue;
+    } else if (input.id === 'cvv') {
+        // Limit to 4 characters for the third input
+        if (inputValue.length > 3) {
+            inputValue = inputValue.substr(0, 3);
+        }
+        input.value = inputValue;
+    }
+}
+
+function limitInputLength(input, maxLength) {
+    if (input.value.length > maxLength) {
+        input.value = input.value.slice(0, maxLength);
+    }
+}
+
 
 
 /*    document.getElementById("submit-btn").onclick = function() {
